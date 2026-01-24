c21328ea19b7548eac36e263d232f3898db8d6ee
Stepan Primshits <xseljvm@gmail.com>
2026-01-25 01:20:05 +0300

    style: simplify user menu

--- components/UserMenu.tsx
+++ components/UserMenu.tsx
@@ -5,9 +5,7 @@ import styles from "./UserMenu.module.css";
 export default function UserMenu() {
   return (
     <div className={styles.userMenu}>
-      <div className={styles.avatar}>AU</div>
       <div className={styles.userInfo}>
-        <span className={styles.userLabel}>Signed in</span>
         <strong className={styles.userName}>Admin User</strong>
         <span className={styles.userEmail}>admin@buzzcut-season.ru</span>
       </div>
